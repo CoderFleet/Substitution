40bb804801ae991fd9401e64b5d4663f644543dc
Rudransh Pratap Singh <hackerboyrps@gmail.com>
2024-07-07 19:25:16 +0530

    Added functionality to reject Invalid Keys

--- substitution.c
+++ substitution.c
@@ -5,6 +5,9 @@
 int main(int argc, string argv[])
 {
 
+    char alphabets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
     // Validate the Key provided and suggest modifications if necessary
     if (argc != 2)
     {
@@ -17,6 +20,30 @@ int main(int argc, string argv[])
         printf("Key must contain 26 characters.\n");
         return 1;
     }
+
+    else
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            int count = 0;
+            for (int i = 0; i < strlen(argv[1]); i++)
+            {
+                if (!isalpha(argv[1][i]))
+                {
+                    printf("Key should only contain alphabetical characters\n");
+                    return 1;
+                }
+                if (alphabets[j] == tolower(argv[1][i]))
+                {
+                    count++;
+                }
+            }
+            if (count != 1)
+            {
+                printf("Enter A Valid Key without character repetition\n");
+                return 1;
+            }
+        }
 
     return 0;
 }
