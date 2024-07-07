73066630aec1e54d7422ddf504252ef3d036af61
Rudransh Pratap Singh <hackerboyrps@gmail.com>
2024-07-07 21:04:34 +0530

    Completed The App (Added Txt Encryption Functionality)

--- substitution.c
+++ substitution.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -17,7 +18,7 @@ int main(int argc, string argv[])
 
     else if (strlen(argv[1]) != 26)
     {
-        printf("Key must contain 26 characters.\n");
+        printf("Key Should be Containing 26 characters.\n");
         return 1;
     }
 
@@ -44,6 +45,42 @@ int main(int argc, string argv[])
                 return 1;
             }
         }
+    }
+
+    // Get Input From user
+    string plaintxt = get_string("plaintext: ");
+
+    // Encrypt The text using key
+    for (int i = 0; i < strlen(plaintxt); i++)
+    {
+        if (isalpha(plaintxt[i]))
+        {
+            int identity = 0;
+            // printf("%i", identity);
+            for (int j = 0; j < 26; j++)
+            {
+                if (tolower(plaintxt[i]) == alphabets[j])
+                {
+                    identity = j;
+                }
+            }
+            // printf("%i", identity);
+            if (isupper(plaintxt[i]))
+            {
+                plaintxt[i] = toupper(argv[1][identity]);
+            }
+            
+
+            else if (islower(plaintxt[i]))
+            {
+                plaintxt[i] = tolower(argv[1][identity]);
+            }
+
+            // pri
+        }
+    }
+
+    printf("ciphertext: %s\n", plaintxt);
 
     return 0;
 }
